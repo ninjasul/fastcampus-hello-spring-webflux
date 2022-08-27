d7f5b8b063c8ee0a28eeac65057c3c0b730c2809
박동엽 <dypark@onestorecorp.com>
2022-08-27 20:28:51 +0900

    BookService 추가

---
+++ src/main/kotlin/com/fastcampus/springwebflux/book/BookService.kt
@@ -0,0 +1,45 @@
+package com.fastcampus.springwebflux.book
+
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import java.util.concurrent.atomic.AtomicInteger
+
+data class Book(val id: Int, val name: String, val price: Int)
+
+@Service
+class BookService {
+    private val nextId = AtomicInteger(0)
+
+    val books = mutableListOf(
+        Book(id = nextId.incrementAndGet(), name = "코틀린 인 액션", price = 30_000),
+        Book(id = nextId.incrementAndGet(), name = "HTTP 완벽 가이드", price = 40_000),
+    )
+
+    fun getAll(): Flux<Book> {
+        return Flux.fromIterable(books)
+    }
+
+    fun get(id: Int): Mono<Book> {
+        return Mono.justOrEmpty(books.find { id == it.id })
+    }
+
+    fun add(request: Map<String, Any>): Mono<Book> {
+        return Mono.just(request)
+            .map { map ->
+                val book = Book(
+                    id = nextId.incrementAndGet(),
+                    name = map["name"].toString(),
+                    price = map["price"] as Int,
+                )
+                books.add(book)
+                book
+            }
+    }
+
+    fun delete(id: Int): Mono<Void> {
+        return Mono.justOrEmpty(books.find { it.id == id })
+            .map { books.remove(it) }
+            .then()
+    }
+}
